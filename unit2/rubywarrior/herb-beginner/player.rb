d2bdb61619c48cae8df92685190683f3fba4505c
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-01-20 17:00:25 -0700

    Beat level 6. Herb is not the brightest potato in the potato bag.

--- unit2/rubywarrior/herb-beginner/player.rb
+++ unit2/rubywarrior/herb-beginner/player.rb
@@ -2,23 +2,27 @@ class Player
   @@health = 20
   def play_turn(warrior)
     @currentHealth = warrior.health
-    if @currentHealth < @@health
+    if @currentHealth < @@health  # => Herb is taking damage
       @@health = @currentHealth
       if warrior.feel.enemy?
         warrior.attack!
-      elsif warrior.feel.captive?
-        warrior.rescue!
-      else
+      elsif @currentHealth < 9
         warrior.walk!(:backward)
+      else
+        warrior.walk!
       end
-    elsif warrior.feel.enemy?
-      warrior.attack!
-    elsif warrior.feel.captive?
-      warrior.rescue!
-    elsif warrior.health < 15
-      warrior.rest!
     else
-      warrior.walk!
+      if warrior.feel.enemy?
+        warrior.attack!
+      elsif @currentHealth < 20
+        @@health = @currentHealth + 2
+        if @@health == 21
+          @@health = 20
+        end
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     end
   end
 end
