9111233e70c753d2099faf5720a03f683b8f88b8
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-01-21 20:47:18 -0700

    Fixed minor issues, decision making seems much better. Still might not always win, but it has won every time with the modified code. Added comments.

--- unit2/rubywarrior/herb-beginner/player.rb
+++ unit2/rubywarrior/herb-beginner/player.rb
@@ -1,21 +1,32 @@
+# => 
+# => Herb player -- by Jeremy Kerr
+# => 
+# => instance vars - shoot, pivot, save, rest, attack, etc..., used in decision making, passed to function choosemove
+# => class var - health and pivotcooldown, defined early and used to prevent too much pivoting and to detect damage
+# => constants - player health - used to manage how much he heals. Herb less likely to heal as he gains health
+# => array - two arrays, store lookforward and lookbackward, used to detect enemies and respond appropriately
+# => hash - no hash
+# => functions - chooseMove - passed numbers for various moves as weights, chooses randomly between them. Higher weights mean better odds.
+# => comments - see below
+
 class Player
-  PLAYER_HEALTH = 20
-  @@health = PLAYER_HEALTH
-  @@pivotCooldown = 0
+  PLAYER_HEALTH = 20  # => used to determine when to heal
+  @@health = PLAYER_HEALTH  # => used to determine when player takes damage
+  @@pivotCooldown = 0 # => used to prevent constant turning
   
-  @@turn = 0
+  @@turn = 0  # => makes warrior pivot once at the beginning of each level
   
   def play_turn(warrior)
-    @currentHealth = warrior.health
-    @lookforward = warrior.look
-    @lookbackward = warrior.look(:backward)
-    @feelFront = warrior.feel
-    @feelBack = warrior.feel(:backward)
+    @currentHealth = warrior.health # => used to determine when player takes damage
+    @lookforward = warrior.look # => used to find enemies/captives
+    @lookbackward = warrior.look(:backward) # => used to find enemies/captives
+    @feelFront = warrior.feel # => allows warrior to attack instead of shoot if enemy is close, or to to anything within one square
+    @feelBack = warrior.feel(:backward) # => intended to be used to free captives behind, but it doesn't quite work right
     
-    @takingDamage = @currentHealth < @@health
-    @@health = @currentHealth
+    @takingDamage = @currentHealth < @@health # => boolean to see when damage is dealt
+    @@health = @currentHealth # => resets so damage doesn't always appear to be happening after first damage
     
-    @@pivotCooldown = @@pivotCooldown - 1
+    @@pivotCooldown = @@pivotCooldown - 1 # => cools down pivot so it can be used again
     
     @shoot = 0
     @pivot = 0
@@ -26,30 +37,30 @@ class Player
     @walk = 0
     @saveBack = 0
     
-    if @lookforward[2].enemy? && @lookbackward[1].enemy?
+    if (@lookforward[2].enemy? && @lookbackward[1].enemy?) || (@lookforward[1].enemy? && @lookbackward[2].enemy?) # => for when enemies are on both sides, to prevent unnecessary turning/backing up
       @shoot = @shoot + 100
     end
     
-    if @feelFront.empty? && !@lookforward[1].enemy?
+    if @feelFront.empty? && !@lookforward[1].enemy? # => for general walking and resting
       @walk = @walk + 1
-      @rest = (PLAYER_HEALTH - @currentHealth) / 2
-    elsif @feelFront.stairs?
-      @walk = @walk + 100
-    elsif @feelFront.enemy?
+      @rest = (PLAYER_HEALTH - @currentHealth) / 2  # => resting less likely as health increases
+    elsif @feelFront.stairs?  # => go to the stairs to win
+      @walk = @walk + 10
+    elsif @feelFront.enemy? # => attack enemies
       @attack = @attack + 1
-    elsif @feelFront.captive?
+    elsif @feelFront.captive? # => free captives
       @save = @save + 15
-    elsif @feelFront.wall?
+    elsif @feelFront.wall?  # => pivot at walls
       @pivot = @pivot + 1
-    elsif @feelFront.ticking?
+    elsif @feelFront.ticking? # => don't chill at bombs
       @goback = 1
     end
     
-    if @lookforward[0].captive?
+    if @lookforward[0].captive? # => save captives
       @save = @save + 10
     end
     
-    if @lookforward[1].enemy? || @lookforward[2].enemy?
+    if @lookforward[1].enemy? || @lookforward[2].enemy? # => don't get to close unless health is good
       if @currentHealth == PLAYER_HEALTH
         @goback = @goback
       else
@@ -57,29 +68,29 @@ class Player
       end
     end
     
-    if @lookforward[2].enemy? && !@lookforward[1].captive? && !@lookforward[0].captive? 
+    if @lookforward[2].enemy? && !@lookforward[1].captive? && !@lookforward[0].captive? # => don't shoot captives
       @shoot = @shoot + 10
     end
     
-    if @lookbackward[1].enemy?
+    if @lookbackward[1].enemy?  # => don't get hit from behind
       if @@pivotCooldown <= 0
         @pivot = @pivot + 10
       end
     end
     
-    if @feelBack.wall?
+    if @feelBack.wall?  # => don't back into walls
       @goback = 0
     end
     
-    if @feelBack.captive?
+    if @feelBack.captive? # => save captives behind you
       @saveback = @saveBack + 10
     end
     
-    if @feelFront.enemy?
+    if @feelFront.enemy?  # => don't shoot enemies in front of you
       @shoot = 0
     end
     
-    if @@turn > 0
+    if @@turn > 0 # => turn around first turn
       chooseMove(@shoot, @pivot, @save, @rest, @attack, @goback, @saveBack, @walk, warrior)
     else
       @@turn = 1
@@ -88,7 +99,7 @@ class Player
   end
   
   def chooseMove(shoot, pivot, save, rest, attack, goback, saveBack, walk, warrior)
-    @range = rand(shoot + pivot + save + rest + attack + saveBack + walk + goback)
+    @range = rand(shoot + pivot + save + rest + attack + saveBack + walk + goback)  # => randomly choose between available weighted options
     if @range < shoot
       warrior.shoot!
     elsif @range < shoot + pivot
