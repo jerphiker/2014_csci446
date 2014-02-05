a52ccdce23477e1730ff99f6c26dbc759cab09e8
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 23:24:46 -0700

    adding unit tests

--- pets/petClearance/test/models/pet_test.rb
+++ pets/petClearance/test/models/pet_test.rb
@@ -4,4 +4,12 @@ class PetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "pet attributes must not be empty" do
+    pet = Pet.newassert pet.invalid?
+    assert pet.errors[:petType].any?
+    assert pet.errors[:name].any?
+    assert pet.errors[:breed].any?
+    assert pet.errors[:description].any?
+    assert pet.errors[:image_url].any?
+  end
 end
