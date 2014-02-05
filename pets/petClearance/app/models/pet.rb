97104a083589d7681fc299a576cf948f6f88866c
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 22:40:32 -0700

    fixed pet.rb

--- pets/petClearance/app/models/pet.rb
+++ pets/petClearance/app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ActiveRecord::Base
-  validates: :name, :breed, :description, :image_url, :petType, presence: true
+  validates :name, :breed, :description, :image_url, :petType, presence: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
