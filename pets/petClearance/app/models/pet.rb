6b849a43aae1d59b1a641073ab9f69137177d42e
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 22:37:55 -0700

    adding validation

--- pets/petClearance/app/models/pet.rb
+++ pets/petClearance/app/models/pet.rb
@@ -1,2 +1,7 @@
 class Pet < ActiveRecord::Base
+  validates: :name, :breed, :description, :image_url, :petType, presence: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
 end
