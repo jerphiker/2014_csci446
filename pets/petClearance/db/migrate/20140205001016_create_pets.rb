cd75357c4527c55e2156b0695f1fea9fa06544d3
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 21:32:54 -0700

    still broken :

--- pets/petClearance/db/migrate/20140205001016_create_pets.rb
+++ pets/petClearance/db/migrate/20140205001016_create_pets.rb
@@ -1,16 +1,13 @@
 class CreatePets < ActiveRecord::Migration
-#  def change
-#    create_table :pets do |t|
-#      t.string :petType
-#      t.string :name
-#      t.string :breed
-#      t.text :description
-#      t.string :image_url
-
-#      t.timestamps
-#    end
-#  end
   def change
-    rename_column :type, :petType, :string
+    create_table :pets do |t|
+      t.string :petType
+      t.string :name
+      t.string :breed
+      t.text :description
+      t.string :image_url
+
+      t.timestamps
+    end
   end
 end
