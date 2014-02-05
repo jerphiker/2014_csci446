c48d48a9647cd52ab75a94564ed7bdf2e8badaa0
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 23:32:46 -0700

    added more tests

--- pets/petClearance/test/models/pet_test.rb
+++ pets/petClearance/test/models/pet_test.rb
@@ -13,4 +13,24 @@ class PetTest < ActiveSupport::TestCase
     assert pet.errors[:description].any?
     assert pet.errors[:image_url].any?
   end
+  
+  def new_pet(image_url)
+    Pet.new(
+      petType: "testType",
+      name: "testName",
+      breed: "testBreed",
+      description: "test test test",
+      image_url: image_url
+    )
+  end
+  test "image url" do
+    ok = %w{ dog.gif dog.jpg dog.png DOG.GIF DOG.Gif http://a.b.c/x/y/z/dog.gif }
+    bad = %w{ dog.doc dog.gif/more dog.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should be invalid"
+    end
+  end
 end
