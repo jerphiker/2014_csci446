31df860c09a4d12721c671fd8923d0212a49019c
Jeremy P. Kerr <jkerr@luna.mines.edu>
2014-02-04 17:05:11 -0700

    Created pets thing

---
+++ pets/petClearance/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetClearance::Application.config.secret_key_base = '9103ca5a16015fc83ab879cdc5b3c1ebf394b935229af4181417ef9238cbb4b575743ff5fabfc5fbdecf6e53ff34e1a152c189f88c3904035f904bbf8e2eaefc'
