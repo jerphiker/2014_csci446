571177486d1a4dc82567777ad54d97982fab9d7b
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 21:43:29 -0700

    fixed

--- pets/petClearance/db/migrate/20140205001016_create_pets.rb
+++ pets/petClearance/db/migrate/20140205001016_create_pets.rb
@@ -1,7 +1,7 @@
 class CreatePets < ActiveRecord::Migration
   def change
     create_table :pets do |t|
-      t.string :petType
+      t.string :type
       t.string :name
       t.string :breed
       t.text :description
