87c521c0bdae0d36d9239394d8e0d1a5da153604
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-04 21:31:07 -0700

    seeds file reaaly bothersome

--- pets/petClearance/db/migrate/20140205001016_create_pets.rb
+++ pets/petClearance/db/migrate/20140205001016_create_pets.rb
@@ -1,13 +1,16 @@
 class CreatePets < ActiveRecord::Migration
-  def change
-    create_table :pets do |t|
-      t.string :petType
-      t.string :name
-      t.string :breed
-      t.text :description
-      t.string :image_url
+#  def change
+#    create_table :pets do |t|
+#      t.string :petType
+#      t.string :name
+#      t.string :breed
+#      t.text :description
+#      t.string :image_url
 
-      t.timestamps
-    end
+#      t.timestamps
+#    end
+#  end
+  def change
+    rename_column :type, :petType, :string
   end
 end
