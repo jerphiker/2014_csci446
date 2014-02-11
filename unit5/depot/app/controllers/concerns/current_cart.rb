37fe099c2dfaa82acc64bd54d401e526b2f97e42
Jeremy Kerr <jkerr@mymail.mines.edu>
2014-02-11 11:18:26 -0700

    added current cart under concerns

---
+++ unit5/depot/app/controllers/concerns/current_cart.rb
@@ -0,0 +1,11 @@
+model CurrentCart
+  extend ActiveSupport::Concern
+  
+  private
+  
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
